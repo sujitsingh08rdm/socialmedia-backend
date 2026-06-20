c1e85fe5893ee3917c791756396c61313c364fde
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2026-06-21 01:42:24 +0530

    nested commented feature added

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -6,6 +6,16 @@ const commentSchema = new mongoose.Schema(
     post: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
     comment: { type: String, required: true },
     commentedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+    taggedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
   },
   { timestamps: true }
 );
